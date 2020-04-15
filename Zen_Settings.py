ea87182b06a509dd4c48808b16253d688ff9b93a
HR-027 <62010656+HR-027@users.noreply.github.com>
2020-04-15 20:06:10 +0100

    Add files via upload

---
+++ Zen_Settings.py
@@ -0,0 +1,30 @@
+# Window Title
+TITLE = "Zen Lawn Mowing"
+
+# Defining some colours I could use
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+ORANGE = (255, 140, 0)
+GREY = (169, 169, 169)
+
+# Initialising the width, height and fps of the game
+WIDTH = 1024
+HEIGHT = 768
+FPS = 60
+
+# Initialise the tile size, grid width and grid height
+# Each tile will be 32 by 32
+TILESIZE = 64
+# 1024/32 = 32 columns
+GRIDWIDTH = WIDTH/TILESIZE
+# 768/32 = 24 rows
+GRIDHEIGHT = HEIGHT/TILESIZE
+
+# Player
+Red_Mower = "Red_Mower.png"
+
+# Mobs
+Mole = "Mole.png"
